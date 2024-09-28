91d6e442a137e7ab6ea432b2242146fe64d82747
Chuckle Head <happymuttel@gmail.com>
2024-09-28 14:03:06 -0400

    success!! background and camera positioning

--- main.js
+++ main.js
@@ -1,71 +1,27 @@
 import * as THREE from "three";
-import { SVGLoader } from "three/examples/jsm/loaders/SVGLoader.js";
-
+// import { SVGLoader } from "three/examples/jsm/loaders/SVGLoader";
 const scene = new THREE.Scene();
-scene.background = new THREE.Color(0xb0b0b0);
-// console.log(scene);
+scene.background = new THREE.Color(0x00ff00);
 
-const helper = new THREE.GridHelper(160, 10, 0x8d8d8d, 0xc1c1c1);
-scene.add(helper);
-helper.rotation.x = Math.PI / 2;
-// console.log(helper);a
-
-// instantiate a loader
-const loader = new SVGLoader();
-
-// load a SVG resource
-loader.load(
-  // resource URL
-  "assets/images/svg/shutter.svg",
-  // called when the resource is loaded
-  function (data) {
-    const paths = data.paths;
-    const group = new THREE.Group();
-
-    for (let i = 0; i < paths.length; i++) {
-      const path = paths[i];
-
-      const material = new THREE.MeshBasicMaterial({
-        color: path.color,
-        side: THREE.DoubleSide,
-        depthWrite: false,
-      });
-
-      const shapes = SVGLoader.createShapes(path);
-
-      for (let j = 0; j < shapes.length; j++) {
-        const shape = shapes[j];
-        const geometry = new THREE.ShapeGeometry(shape);
-        const mesh = new THREE.Mesh(geometry, material);
-        group.add(mesh);
-      }
-    }
-
-    scene.add(group);
-  }
-  // called when loading is in progresses
-  // function ( xhr ) {
-
-  // 	console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-
-  // },
-  // // called when loading has errors
-  // function ( error ) {
-
-  // 	console.log( 'An error happened' );
-
-  // }
+const camera = new THREE.PerspectiveCamera(
+  20,
+  window.innerWidth / window.innerHeight,
+  1,
+  1000
 );
 
-// const loader = new SVGLoader();
-// console.log(loader);
+camera.position.set(0, 0, 10);
+// camera.lookAt(0, 3, 12);
+// console.log(scene);
 
-// loader.load( url, function ( /*data*/ ) {
-//         // scene.add(data);
-// 				const group = new THREE.Group();
-// 				group.scale.multiplyScalar( 0.25 );
-// 				group.position.x = - 70;
-// 				group.position.y = 70;
-// 				group.scale.y *= - 1;
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff00ff });
+const cube = new THREE.Mesh(geometry, material);
+const renderer = new THREE.WebGLRenderer();
+scene.add(cube);
 
-// 				let renderOrder = 0;
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+console.log(THREE);
+renderer.render(scene, camera);
---
+++ tests/test_one.js
@@ -0,0 +1,78 @@
+import * as THREE from "three";
+import { SVGLoader } from "three/examples/jsm/loaders/SVGLoader";
+
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0xb0b0b0);
+console.log(scene);
+
+// const helper = new THREE.GridHelper(160, 10, 0x8d8d8d, 0xc1c1c1);
+// scene.add(helper);
+// helper.rotation.x = Math.PI / 2;
+// console.log(helper);a
+const container = document.getElementById("container");
+// instantiate a loader
+const loader = new SVGLoader();
+
+// load a SVG resource
+loader.load(
+  // resource URL
+  "assets/images/svg/shutter.svg",
+  // called when the resource is loaded
+  function (data) {
+    const paths = data.paths;
+    const group = new THREE.Group();
+
+    for (let i = 0; i < paths.length; i++) {
+      const path = paths[i];
+
+      const material = new THREE.MeshBasicMaterial({
+        color: path.color,
+        side: THREE.DoubleSide,
+        depthWrite: false,
+      });
+
+      const shapes = SVGLoader.createShapes(path);
+
+      for (let j = 0; j < shapes.length; j++) {
+        const shape = shapes[j];
+        const geometry = new THREE.ShapeGeometry(shape);
+        const mesh = new THREE.Mesh(geometry, material);
+        group.add(mesh);
+      }
+    }
+
+    scene.add(group);
+  },
+  // called when loading is in progresses
+  function (xhr) {
+    console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+  },
+  // called when loading has errors
+  function (error) {
+    console.log("An error happened");
+  }
+);
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+
+function render() {
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  container.appendChild(renderer.domElement);
+}
+// renderer = new THREE.WebGLRenderer();
+
+// render();
+
+// const loader = new SVGLoader();
+// console.log(loader);
+
+// loader.load( url, function ( /*data*/ ) {
+//         // scene.add(data);
+// 				const group = new THREE.Group();
+// 				group.scale.multiplyScalar( 0.25 );
+// 				group.position.x = - 70;
+// 				group.position.y = 70;
+// 				group.scale.y *= - 1;
+
+// 				let renderOrder = 0;
