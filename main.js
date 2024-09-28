34f678afd2acfd36839bb6503a33764c230c7e11
Chuckle Head <happymuttel@gmail.com>
2024-09-28 11:06:23 -0400

    SVG Setup

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
-import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.169.0/build/three.module.js';
+import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.169.0/build/three.module.js";
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
-const renderer = new THREE.WebGLRenderer();
-
 console.log(scene);
-console.log(renderer);
-console.log(camera);
+// const camera = new THREE.PerspectiveCamera(
+//   75,
+//   window.innerWidth / window.innerHeight,
+//   1,
+//   1000
+// );
+// camera.position.set(0, 0, 200);
+
+// renderer = new THREE.WebGLRenderer({ antialias: true });
+// renderer.setPixelRatio(window.devicePixelRatio);
+// renderer.setSize(window.innerWidth, window.innerHeight);
+// container.appendChild(renderer.domElement);
+
+// const controls = new OrbitControls(camera, renderer.domElement);
+// controls.addEventListener("change", render);
+// controls.screenSpacePanning = true;
+
+// window.addEventListener("resize", onWindowResize);
